95714fef47b293e8f3686c5a06a3538ddeb1459c
Gor <58237309+Cactus322@users.noreply.github.com>
2022-07-14 17:50:15 +0300

    refactor project structure

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blogs')
+
+mongoose.connect(config.MONGODB_URL)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB: ', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
