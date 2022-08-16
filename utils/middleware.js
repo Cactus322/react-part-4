f2b434e068cc438784a82befcc0e11c55da84586
Gor <58237309+Cactus322@users.noreply.github.com>
2022-08-16 23:56:52 +0300

    update middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,3 +1,19 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+const logger = require('./logger')
+
+const errorHandler = (error, request, response, next) => {
+    logger.error(error.message)
+
+    if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({
+            error: 'invalid token'
+        })
+    }
+
+    next(error)
+}
+
 const tokenExtractor = async (request, response, next) => {
     const authorization = await request.get('authorization')
     if (authorization && authorization.toLowerCase().startsWith('bearer')) {
@@ -9,4 +25,18 @@ const tokenExtractor = async (request, response, next) => {
     next()
 }
 
-module.exports = tokenExtractor
+const userExtractor = async (request, response, next) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    request.user = await User.findById(decodedToken.id)
+
+    next()
+}
+
+module.exports =  {
+    errorHandler,
+    tokenExtractor,
+    userExtractor
+}
